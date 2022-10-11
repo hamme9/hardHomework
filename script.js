7d8e748c739686aa61532ae68a7808789d12ff54
hamme9 <arseniy-2002@bk.ru>
2022-10-11 17:46:09 +0700

    changed work function

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ let argumentThree = "Lorem ipsum dolor sit amet consectetur adipisicing elit";
 
 const myFunc = function(arg) {
     if (typeof arg !== 'string') {
-        return "В качестве аргумента передана не строка!";
-    } else if (typeof arg === "string" && arg.length <= 30) {
-        return arg.trim();
-    } else if (arg.length > 30) {
-        return arg.slice(0, 30) + '...';
+        console.log("В качестве аргумента передана не строка!");
+        return;
     }
+    let result = arg.trim();
+    return result.length > 30 ? result.slice(0, 30) + '...' : result;
 };
 
 console.log(myFunc(argumentOne));
