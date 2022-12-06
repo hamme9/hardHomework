fb517a3501a78555351460030e17e96153a1a13e
hamme9 <arseniy-2002@bk.ru>
2022-12-07 02:37:08 +0700

    reworking func

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 'use strict';
 
-const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const todayDay = new Date();
+let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', '<i>Saturday</i>', '<i>Sunday</i>'];
+let today = new Date().getDay();
+let day = (6 + today) % 7;
 
-week.forEach(function(item, i){
-    if (i == todayDay.getDay() - 1 && item == 'Saturday' || todayDay.getDay() == 0 && item == 'Sunday') {
-        document.body.insertAdjacentHTML("beforeend", "<div><b><i>" + week[i] + "</i></b></div>");
-    }  else if (item == 'Saturday' || item == 'Sunday') {
-        document.body.insertAdjacentHTML("beforeend", "<div><i>" + week[i] + "</i></div>");
-    }else if (i == todayDay.getDay() - 1){
-        document.body.insertAdjacentHTML("beforeend", "<div><b>" + week[i] + "</b></div>");
-    } else {
-        document.body.insertAdjacentHTML("beforeend", "<div>" + week[i] + "</div>");
-    }
-});
+week[day] = '<b>' + week[day] + '</b>';
+  
+document.body.insertAdjacentHTML("beforeend", week.join("<p>"));
