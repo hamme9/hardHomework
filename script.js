4ec751de573a80da4867763251148ada146d3d07
hamme9 <arseniy-2002@bk.ru>
2022-12-06 22:58:25 +0700

    Made 2 types of timer

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 'use strict';
+const timer = () => {
+    let dateNow = new Date()
+    const timeToday = document.querySelector('.time-today')
+    const timeNow = document.querySelector('.time-now')
 
-let argumentOne = "  Привет";
-let argumentTwo = 12;
-let argumentThree = "Lorem ipsum dolor sit amet consectetur adipisicing elit";
+    const hoursTest = dateNow.getHours().toString().slice(-1)
+    const minutesTest = dateNow.getMinutes().toString().slice(-1)
+    const secondsTest = dateNow.getSeconds().toString().slice(-1)
 
-const myFunc = function(arg) {
-    if (typeof arg !== 'string') {
-        return "В качестве аргумента передана не строка!";
-    } else if (typeof arg === "string" && arg.length <= 30) {
-        return arg.trim();
-    } else if (arg.length > 30) {
-        return arg.slice(0, 30) + '...';
+    let fullDate
+    let hours = dateNow.getHours()
+    let minutes = dateNow.getMinutes()
+    let seconds = dateNow.getSeconds()
+    const options = { 
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long'
     }
-};
 
-console.log(myFunc(argumentOne));
-console.log(myFunc(argumentTwo));
-console.log(myFunc(argumentThree));
+    const dateRu = dateNow.toLocaleString('ru-RU', options);
+
+    fullDate = dateRu.charAt(0).toUpperCase() + dateRu.slice(1)
+
+    if(hoursTest>= '5' || hoursTest== '0') hours +=  ' часов ';
+    if(hoursTest== '1') hours +=  ' час ';
+    if(hoursTest>= '2' && hoursTest<= '4') hours +=  ' часа ';
+
+    if(minutesTest>= '5' || minutesTest== '0') minutes += ' минут ';
+    if(minutesTest== '1') minutes += ' минута ';
+    if(minutesTest>= '2' && minutesTest<= '4') minutes += ' минуты ';
+
+    if(secondsTest>= '5' || secondsTest== '0') seconds += ' секунд ';
+    if(secondsTest== '1') seconds += ' секунда ';
+    if(secondsTest>= '2' && secondsTest<= '4') seconds += ' секунды ';
+
+    timeToday.textContent = fullDate + ', ' + hours + minutes + seconds
+
+    hours = dateNow.getHours()
+    minutes = dateNow.getMinutes()
+    seconds = dateNow.getSeconds()
+
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+    hours = (hours < 10) ? '0' + hours : hours;
+
+    timeNow.textContent = dateNow.toLocaleDateString() + ' - ' + hours + ':' + minutes + ':' + seconds
+
+}
+timer()
+setInterval(() => {
+    timer()
+}, 1000);
