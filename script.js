eeb17cd689dd2ddc5c54310cc492c95ab04470d4
hamme9 <arseniy-2002@bk.ru>
2022-12-07 01:42:53 +0700

    fix bugs

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 const todayDay = new Date();
 
 week.forEach(function(item, i){
-    console.log(item + ' ' + i);
-    
-    if (i == todayDay.getDay() - 1){
-        document.body.insertAdjacentHTML("beforeend", "<div><b>" + week[i] + "<b></div>");
-    } else if (item == 'Saturday' || item == 'Sunday') {
+    if (i == todayDay.getDay() - 1 && item == 'Saturday' || todayDay.getDay() == 0 && item == 'Sunday') {
+        document.body.insertAdjacentHTML("beforeend", "<div><b><i>" + week[i] + "</i></b></div>");
+    }  else if (item == 'Saturday' || item == 'Sunday') {
         document.body.insertAdjacentHTML("beforeend", "<div><i>" + week[i] + "</i></div>");
+    }else if (i == todayDay.getDay() - 1){
+        document.body.insertAdjacentHTML("beforeend", "<div><b>" + week[i] + "</b></div>");
     } else {
         document.body.insertAdjacentHTML("beforeend", "<div>" + week[i] + "</div>");
     }
