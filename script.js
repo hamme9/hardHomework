04ff633b5b07a27dbfbea63e4f97f17d95de861f
hamme9 <arseniy-2002@bk.ru>
2022-12-08 23:55:38 +0700

    made a function for substituting the declination depending on the number

--- script.js
+++ script.js
@@ -1,45 +1,32 @@
 'use strict';
+
+
+const timeToday = document.querySelector('.time-today')
+const timeNow = document.querySelector('.time-now')
+
+const hoursTest = ['час','часа','часов']
+const minutesTest = ['минута','минуты','минут']
+const secondsTest = ['секунда','секунды','секунд']
+
 const timer = () => {
     let dateNow = new Date()
-    const timeToday = document.querySelector('.time-today')
-    const timeNow = document.querySelector('.time-now')
-
-    const hoursTest = dateNow.getHours().toString().slice(-1)
-    const minutesTest = dateNow.getMinutes().toString().slice(-1)
-    const secondsTest = dateNow.getSeconds().toString().slice(-1)
-
     let fullDate
     let hours = dateNow.getHours()
     let minutes = dateNow.getMinutes()
     let seconds = dateNow.getSeconds()
+
     const options = { 
         year: 'numeric',
         month: 'long',
         day: 'numeric',
         weekday: 'long'
     }
-
+    
     const dateRu = dateNow.toLocaleString('ru-RU', options);
 
-    fullDate = dateRu.charAt(0).toUpperCase() + dateRu.slice(1)
+    fullDate = (dateRu.charAt(0).toUpperCase() + dateRu.slice(1)).replace(/г.$/gm, 'год')
 
-    if(hoursTest>= '5' || hoursTest== '0') hours +=  ' часов ';
-    if(hoursTest== '1') hours +=  ' час ';
-    if(hoursTest>= '2' && hoursTest<= '4') hours +=  ' часа ';
-
-    if(minutesTest>= '5' || minutesTest== '0') minutes += ' минут ';
-    if(minutesTest== '1') minutes += ' минута ';
-    if(minutesTest>= '2' && minutesTest<= '4') minutes += ' минуты ';
-
-    if(secondsTest>= '5' || secondsTest== '0') seconds += ' секунд ';
-    if(secondsTest== '1') seconds += ' секунда ';
-    if(secondsTest>= '2' && secondsTest<= '4') seconds += ' секунды ';
-
-    timeToday.textContent = fullDate + ', ' + hours + minutes + seconds
-
-    hours = dateNow.getHours()
-    minutes = dateNow.getMinutes()
-    seconds = dateNow.getSeconds()
+    timeToday.textContent = fullDate + ', ' + addEnd(hours, hoursTest, hours) + ' ' + addEnd(minutes, minutesTest, minutes) + ' ' + addEnd(seconds, secondsTest, seconds)
 
     minutes = (minutes < 10) ? '0' + minutes : minutes;
     seconds = (seconds < 10) ? '0' + seconds : seconds;
@@ -48,6 +35,16 @@ const timer = () => {
     timeNow.textContent = dateNow.toLocaleDateString() + ' - ' + hours + ':' + minutes + ':' + seconds
 
 }
+
+const addEnd = (num, arr, time) => {
+    num = Math.abs(num) % 100; 
+    let count = num % 10;
+    if(num > 10 && num < 20) return time + ' ' + arr[2]; 
+    if(count > 1 && count < 5) return time + ' ' + arr[1];
+    if(count == 1) return time + ' ' + arr[0]; 
+    return time + ' ' + arr[2];
+}
+
 timer()
 setInterval(() => {
     timer()
